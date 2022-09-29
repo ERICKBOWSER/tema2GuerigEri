cc9a9fa4b363d8b6481279b153ce9fb7dd1bde65
Guerig Eri <guerig.eri@gmail.com>
2022-09-29 22:34:31 +0200

    Ej06 modificado y añadido Ej14

--- src/main/java/expresioneslogicas/EjemplosExpresionesLogicas.java
+++ src/main/java/expresioneslogicas/EjemplosExpresionesLogicas.java
@@ -11,62 +11,63 @@ import java.util.Scanner;
  * @author ERICK
  */
 public class EjemplosExpresionesLogicas {
+    public static void main(String[] args){
 
-    /*
-    Leer por teclado nombre y apellidos de una persona
-    edad, peso y estatura
+        /*
+        Leer por teclado nombre y apellidos de una persona
+        edad, peso y estatura
+
+        Guardar en variables booleanas el resultado de evaluar las siguientes expresiones
+        Usa equalsIgnoreCase de los Strings
+        */
+        String nombre, apellidos;
+        int edad;
+        double peso, estatura;
+
+        // Objeto scanner para la llamada por teclado
+        Scanner teclado = new Scanner(System.in);
+
+
+        // EDAD
+        // Mensaje por pantalla para solicitar la edad
+        System.out.println("Introduce tu edad: ");
+
+        // Almacenamiento del valor pasado por consola
+        edad = teclado.nextInt();
+        System.out.println("Su edad es " + edad);
+
+        /*Limpiamos el buffer del contenido que ya tenía previamente en la anterior consulta*/
+        teclado.nextLine();
+
+        // PESO
+        System.out.println("Introduce tu edad: ");
+
+        peso = teclado.nextDouble();
+        System.out.println("Su peso es " + peso);
+        teclado.nextLine();
+
+        // ESTATURA
+
+        System.out.println("Introduce tu estatura: ");
+
+        estatura = teclado.nextDouble();
+        System.out.println("Su estatura es " + estatura);
+
+        teclado.nextLine();
+
+
+        // NOMBRE
+        System.out.println("Introduce tu nombre: ");
+        nombre = teclado.nextString();
+
+        teclado.nextLine();
+
+        System.out.println("Introduce tus apellidos: ");
+        apellidos = teclado.nextString();
+
+        System.out.println("Su nombre completo es " + nombre + " " + apellidos);
     
-    Guardar en variables booleanas el resultado de evaluar las siguientes expresiones
-    Usa equalsIgnoreCase de los Strings
-    */
-    String nombre, apeelidos;
-    int edad;
-    double peso, estatura;
-    
-    // Objeto scanner para la llamada por teclado
-    Scanner teclado = new Scanner(System.in);
-    
-    
-    // EDAD
-    // Mensaje por pantalla para solicitar la edad
-    System.out.println("Introduce tu edad: ");
-    
-    // Almacenamiento del valor pasado por consola
-    edad = teclado.nextInt();
-    System.out.println("Su edad es " + edad);
-    
-    /*Limpiamos el buffer del contenido que ya tenía previamente en la anterior consulta*/
-    teclado.nextLine();
-    
-    // PESO
-    System.out.println("Introduce tu edad: ");
-    
-    peso = teclado.nextDouble();
-    System.out.println("Su peso es " + peso);
-    teclado.nextLine();
-    
-    // ESTATURA
-   
-    System.out.println("Introduce tu estatura: ");
-    
-    estatura = teclado.nextDouble();
-    System.out.println("Su estatura es " + estatura);
-    
-    teclado.nextLine();
-    
-    
-    // NOMBRE
-    System.out.println("Introduce tu nombre: ");
-    nombre = teclado.nextString();
-    
-    teclado.nextLine();
-    
-    System.out.println("Introduce tus apellidos: ");
-    apellidos = teclado.nextString();
-    
-    System.out.println("Su nombre completo es " + nombre + " " + apellidos);
-    
-    
+    }
 }
 
 
